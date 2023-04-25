d27807467faa4230901d325c14df80f0845cbd51
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-04-25 22:18:01 +0430

    Create and customize admin model

--- RertoProject/MVP/admin.py
+++ RertoProject/MVP/admin.py
@@ -4,6 +4,19 @@ from .models import Retro,Profile,RetroNote,User
 
 # Register your models here.
 
-admin.site.register(Retro)
+# admin.site.register(Retro)
 admin.site.register(Profile)
 admin.site.register(RetroNote)
+
+@admin.register(Retro)
+class RetroAdmin(admin.ModelAdmin):
+    list_display = ['Sprint_Number','retro_date','position']
+    list_filter = ['Sprint_Number','retro_date','position']
+    search_fields = ['Sprint_Number','retro_date','position']
+    ordering = ['Sprint_Number','retro_date','position']
+
+
+# @admin.register(RetroNote)
+# class RetroNoteAdmin(admin.ModelAdmin):
+#     list_display = ['person','retro','vote']
+#got error, i cant define multiple admin models 
