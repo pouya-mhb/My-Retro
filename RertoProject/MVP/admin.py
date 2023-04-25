6bfcde63a29307cefa3c181321aa1a9e80917b84
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-04-25 21:08:09 +0430

    create models and register  in admin

--- RertoProject/MVP/admin.py
+++ RertoProject/MVP/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Retro,Profile,RetroNote,User
+
 # Register your models here.
+
+admin.site.register(Retro)
+admin.site.register(Profile)
+admin.site.register(RetroNote)
--- RertoProject/MVP/models.py
+++ RertoProject/MVP/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    gender_choices = (
+        ('M','Male'),
+        ('F','Female')
+    )
+
+    first_name = models.CharField(max_length=100,default='User_first_name')
+    last_name = models.CharField(max_length=100,default='user_last_name')
+    age = models.IntegerField(default=18)
+    gender = models.CharField(choices=gender_choices,max_length=6)
+    
+class Retro (models.Model):
+    team_choices = (
+        ('Front-end','Front-end'),
+        ('Back-end','Back-end'),
+        ('Product','Product'),
+        ('Support','Support'),
+        ('Sale','Sale'),
+        ('Managers','Managers')
+    )
+    person = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    position = models.CharField(choices=team_choices,max_length=10)
+    Retro_number = models.IntegerField ()
+    retro_date = models.DateTimeField (default=timezone.now)
+
+    class Meta :
+        def __str__(self) -> str:
+            return self.date_time
+
+class RetroNote (models.Model):
+    vote_choices = (
+        ('Agree','Agree'),
+        ('Disagree','Disagree')
+    )
+
+    likes = models.TextField (max_length=250)
+    learns = models.TextField(max_length=250)
+    lacks = models.TextField(max_length=250)
+    mad = models.TextField(max_length=250)
+    glad = models.TextField(max_length=250)
+    sad = models.TextField(max_length=250)
+    vote = models.CharField(choices=vote_choices,max_length=10)
